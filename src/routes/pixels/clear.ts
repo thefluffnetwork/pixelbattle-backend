53fb321975cb4b2df1e49ad60cd4fb3a9a27d5c6
bgenia <xbgenia@gmail.com>
2025-05-01 03:56:21 +0300

    feat: make pixels/clear send canvas reset event

--- src/routes/pixels/clear.ts
+++ src/routes/pixels/clear.ts
@@ -1,6 +1,9 @@
 import type { IncomingMessage, Server, ServerResponse } from "node:http"
 import type { RouteOptions } from "fastify"
+import { WebSocket } from "ws"
+import { toJson } from "../../extra/toJson"
 import { genericSuccessResponse } from "../../types/ApiReponse"
+import type { SocketPayload } from "../../types/SocketActions"
 
 interface Body {
   color: string
@@ -36,6 +39,14 @@ export const clear: RouteOptions<
       height: request.server.game.height,
     }
     await request.server.cache.canvasManager.clear(color)
+
+    for (const client of request.server.websocketServer.clients) {
+      if (client.readyState !== WebSocket.OPEN) continue
+
+      const payload: SocketPayload<"RESET"> = { op: "RESET" }
+
+      client.send(toJson(payload))
+    }
 
     return response.code(200).send({ ...genericSuccessResponse, canvas })
   },
